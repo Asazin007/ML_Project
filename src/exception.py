0e3c9f840775cd680d2324cd188d004a98650f89
Asazin007 <asim80822@gmail.com>
2023-06-30 17:04:52 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 # provide additional information about the exception that occurred
 
 def error_message_detail(error,error_detail:sys):
@@ -19,4 +19,11 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+# if __name__== "__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by 0 error")
+#         raise CustomException(e, sys)
     
